3061af70b4e5296799c6e38e8c040c1f79498dc4
kaikySantos <info.kaiky@gmail.com>
2022-06-17 15:59:56 -0300

    feat: feature component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { Flex } from "@chakra-ui/react";
-import Banner from "../components/Banner";
 
 import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Features from "../components/Features";
 
 export default function Home() {
   return (
     <Flex direction="column">
       <Header />
       <Banner />
+      <Features />
     </Flex>
   )
 }
